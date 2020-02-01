b9662ae9b2ec530f9962f7d33f9565c5ce10f438
Costin Dragoi <csdcostin@gmail.com>
2020-02-01 11:54:24 +0100

    (UPD)

--- src/ninixexe/core.clj
+++ src/ninixexe/core.clj
@@ -4,23 +4,6 @@
 
 (defn n [n] (midi->hz (note n)))
 
-(demo (sin-osc 80))
-(demo
- (let [env (env-gen (perc 0 0.6))]
-   (* (sin-osc 80) env)))
-(demo
- (let [env (env-gen (perc 0 0.6))]
-   (* 2 (sin-osc (+ 40 (* env env env 200))) env)))
-(demo
- (let [env (env-gen (perc 0 0.6))
-       snare (* (pink-noise) env)]
-   snare))
-(demo
- (let [env (env-gen (perc 0 0.6))
-       snare (* (pink-noise) env)
-       snare (+ snare (bpf (* 4 snare) 2000))]
-   snare))
-
 (definst kick []
   (let [env (env-gen (perc 0 0.6))]
     (* (sin-osc (+ 40 (* env env env 200))) env)))
@@ -33,12 +16,24 @@
 
 (defonce metro (metronome 120))
 
-(at (metro (+ (metro) 0)) (kick))
-(at (metro (+ (metro) 1)) (snare))
+(def bar
+  (let [wobble [[0 #(ctl wob :freq (n :e1) :speed 1)]
+                [0 #(ctl wob :speed 2)]
+                [0 #(ctl wob :speed 4)]
+                [0 #(ctl wob :speed 8)]
+                [0 #(ctl wob :freq (n :f#5) :speed 8)]
+                [0 #(ctl wob :freq (n :e2) :speed 2)]
+                [0 #(ctl wob :speed 4)]
+                [0 #(ctl wob :speed 8)]]
 
-(def bar [[0 kick ]
-          [0 snare]])
+        drum [[  0   kick]
+              [  0   snare
+               2/4   kick]
+              [  0   kick
+               3/4   kick]
+              [  0   snare]]]
 
+    (mapv concat wobble (cycle drum))))
 
 (defn player [tick]
   (let [beat (mod (int tick) (count bar))]
@@ -60,32 +55,12 @@
                (* 0.2 (g-verb wob 9 0.7 0.7)))]
     wob))
 
-(def bar
-  (let [wobble [[0 #(ctl wob :freq (n :e1) :speed 1)]
-                [0 #(ctl wob :speed 2)]
-                [0 #(ctl wob :speed 4)]
-                [0 #(ctl wob :speed 8)]
-                [0 #(ctl wob :freq (n :f#5) :speed 8)]
-                [0 #(ctl wob :freq (n :e2) :speed 2)]
-                [0 #(ctl wob :speed 4)]
-                [0 #(ctl wob :speed 8)]]
-
-        drum [[  0   kick]
-              [  0   snare
-               2/4   kick]
-              [  0   kick
-               3/4   kick]
-              [  0   snare]]]
-
-    (mapv concat wobble (cycle drum))))
-
-
 
 (defn -main
   "Main class."
   [& args]
 
-  (println "LETS START THE PARTY! \n")
+  (println "\n\nLETS START THE PARTY!\n")
   (println "<CTRL + C> to stop")
   ;(stop)
   (player (metro))
